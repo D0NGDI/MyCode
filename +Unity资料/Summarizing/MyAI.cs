77d8bc6df2b441b92d1821bac4ec61614e5c3750
D0NGDI <36936773+D0NGDI@users.noreply.github.com>
2019-05-01 01:07:17 +0800

    Add files via upload

---
+++ +Unity资料/Summarizing/MyAI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.AI;
+
+public class MyAI : PausingBehaviour//MonoBehaviour
+{
+    protected override void OnUpdate()
+    {
+        
+        Debug.Log("{OnUpdate} pause = " + PausingBehaviour.pause);
+        //transform.rotation = Quaternion.AngleAxis(180 , Vector3.up);
+        transform.Rotate(0, 8, 0, Space.Self);//自身坐标Z轴旋转
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PausingBehaviour.pause = !PausingBehaviour.pause;
+        }
+
+    }
+}
